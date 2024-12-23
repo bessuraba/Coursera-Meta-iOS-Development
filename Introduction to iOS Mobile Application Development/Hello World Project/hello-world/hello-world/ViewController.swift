7686f20b16d65f0627ddc5e8ac4e942479127d28
Slava Losev <slawa.bessuraba@gmail.com>
2024-12-23 02:38:55 +0100

    feat: module 1

---
+++ Introduction to iOS Mobile Application Development/Hello World Project/hello-world/hello-world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello-world
+//
+//  Created by Слава Лосев on 23.12.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
